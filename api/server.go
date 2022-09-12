c2bfc31322b8c6465ff8b8f04804144e130348c7
umarhadi <hi@umarhadi.dev>
2022-09-12 16:15:47 +0800

    api(feat): login users
    Signed-off-by: umarhadi <hi@umarhadi.dev>

--- api/server.go
+++ api/server.go
@@ -28,22 +28,27 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 		store:      store,
 		tokenMaker: tokenMaker,
 	}
-	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 
 	router.POST("/transfers", server.createTransfer)
-
 	server.router = router
-	return server, nil
 }
 
 func (server *Server) Start(address string) error {

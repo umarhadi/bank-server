4fef8001f7bc69b6a0416c79b5d370d79a40ca23
umarhadi <hi@umarhadi.dev>
2022-09-19 13:41:46 +0800

    grpc: add create & login user
    Signed-off-by: umarhadi <hi@umarhadi.dev>

---
+++ gapi/converter.go
@@ -0,0 +1,17 @@
+package gapi
+
+import (
+	db "github.com/umarhadi/bank-server/db/sqlc"
+	"github.com/umarhadi/bank-server/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
---
+++ gapi/rpc_create_user.go
@@ -0,0 +1,41 @@
+package gapi
+
+import (
+	"context"
+
+	"github.com/lib/pq"
+	db "github.com/umarhadi/bank-server/db/sqlc"
+	"github.com/umarhadi/bank-server/pb"
+	"github.com/umarhadi/bank-server/util"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	hashedPassword, err := util.HashPassword(req.GetPassword())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.GetUsername(),
+		HashedPassword: hashedPassword,
+		FullName:       req.GetFullName(),
+		Email:          req.GetEmail(),
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				return nil, status.Errorf(codes.AlreadyExists, "username already exists: %s", err)
+			}
+		}
+		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+	rsp := &pb.CreateUserResponse{
+		User: convertUser(user),
+	}
+	return rsp, nil
+}

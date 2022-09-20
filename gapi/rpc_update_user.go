77d2cbb44981b56866805818fe62efcffda910d4
umarhadi <hi@umarhadi.dev>
2022-09-20 12:11:47 +0800

    grpc: add authorization update user
    Signed-off-by: umarhadi <hi@umarhadi.dev>

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -15,9 +15,18 @@ import (
 )
 
 func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	authPayload, err := server.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
 	violations := validateUpdateUserRequest(req)
 	if violations != nil {
 		return nil, invalidArgumentError(violations)
+	}
+
+	if authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "you can only update your own account")
 	}
 
 	arg := db.UpdateUserParams{

dc3dedf52b5c77991d0903b211bfc2aefcb913b7
umarhadi <hi@umarhadi.dev>
2022-10-31 14:21:05 +0700

    add http logger middleware
    Signed-off-by: umarhadi <hi@umarhadi.dev>

--- main.go
+++ main.go
@@ -126,6 +126,8 @@ func runGatewayServer(config util.Config, store db.Store) {
 	}
 
 	log.Info().Msgf("start HTTP gateway server at %s", listener.Addr().String())
+	handler := gapi.HttpLogger(mux)
+	err = http.Serve(listener, handler)
 	err = http.Serve(listener, mux)
 	if err != nil {
 		log.Fatal().Err(err).Msg("cannot start HTTP gateway server")
